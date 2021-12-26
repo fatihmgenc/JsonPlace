05926a8191efd3fef62682a917714a227aeeeff5
fatihmgenc <fatihmgenc@outlook.com>
2021-12-27 00:44:23 +0300

    Authorization start

---
+++ JsonPlaceApi/Helpers/JwtTokenValidator .cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JsonPlaceApi.Helpers
+{
+    public class JwtTokenValidator : ISecurityTokenValidator
+    {
+        public bool CanReadToken(string securityToken) => true;
+
+        public ClaimsPrincipal ValidateToken(string securityToken, TokenValidationParameters validationParameters, out SecurityToken validatedToken)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "Fatih",
+                ValidAudience = "fatihmgenc",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("RlSg8qAvQSTTIuKDTEcGpvXu9qmzsPj764UjY3GgkFtnA1v07yoGT8qBc9AC6ff"))
+            };
+
+            var claimsPrincipal = handler.ValidateToken(securityToken, tokenValidationParameters, out validatedToken);
+            return claimsPrincipal;
+        }
+
+        public bool CanValidateToken { get; } = true;
+        public int MaximumTokenSizeInBytes { get; set; } = int.MaxValue;
+    }
+}
--- JsonPlaceApi/Program.cs
+++ JsonPlaceApi/Program.cs
@@ -1,5 +1,7 @@
 using JsonPlace.Business;
+using JsonPlaceApi.Helpers;
 using JsonPlaceApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MongoDB.Bson;
 using MongoDB.Bson.IO;
 using MongoDB.Driver;
@@ -30,12 +32,15 @@ builder.Services.AddSingleton<IMongoClient>(sp =>
 
     return new MongoClient(mongoClientSettings);
 });
+
 BusinessDIModule.Inject(builder.Services, builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapGrpcService<GreeterService>();
 app.MapGrpcService<TokenService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
 app.Run();
